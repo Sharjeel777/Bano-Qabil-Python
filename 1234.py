0710555c248dec0c090d1b8173052372ac699e87
Sharjeel777 <157410287+Sharjeel777@users.noreply.github.com>
2024-01-31 09:24:03 -0800

    Add files via upload

---
+++ 1234.py
@@ -0,0 +1,64 @@
+num = int(input("enter the number."))
+if num%2==0:
+    print("the given number is an even number")
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+c=int(input("enter c:"))
+if a>b and a>c:
+ print("from the above three numbers given a is largest")
+if b>a and b>c:
+ print("from the above three numbers given b is largest")
+if c>b and c>a:
+ print("from the above three numbers given c is largest")
+age=int(input("enter age:"))
+if age >= 18:
+    print("you are eligible to vote !!")
+else:
+    print("sorry! you have to wait !")
+number=int(input("enter the number?"))
+if number == 10:
+    print ("the given number is equal to 10")
+elif number == 50:
+      print ("the given number is equal to 50")
+elif number == 100:
+      print ("the given number is equal to 100")
+else:
+    print ("the given number is not equal to 10,50 and 100")
+    languages = ['Swift','Python','Go','Java Script']
+for count in languages:
+    print(count)
+number = 0
+while number < 100:
+    print(f"number is {number}!")
+    number = number + 1
+import math
+x = math.ceil (100)
+y = math.sqrt (100)
+z = math.floor (100)
+print (x)
+print (y)
+print (z)
+def fun():
+    print("Bano Qabil")
+fun()
+fun()
+fun()
+fun()
+fun()
+fun()
+fun()
+fun()
+return bool (a)
+res = add(2, 3)
+print("result of add function is {} ".format(res))
+res = is true (2< 5)
+print("\nresult of is true function is {}" .format(res))
+ages = {"Ali": 50 , "Hamza": 30, "Rehan Ullah":40}
+for student in ages:
+    print(student)
+    ages= [ 19,20,25]
+print(ages)
+languages = ["python", "swift","c++"]
+print(languages[0])
+list = ['p','r','o','g','r','a','m','i','z']
+print (list[2:6])
